36bba38dbc743df3f19bf36a938592553fc7ce56
Joost Yervante Damad <joost@damad.be>
2018-04-04 13:30:04 +0200

    use python's introspective qualities

---
+++ src/prelude.py
@@ -0,0 +1,26 @@
+def flatten(S):
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
+
+class Element:
+    def __init__(self, t):
+        self.t = t
+        
+    def generate(self):
+        return self.__dict__
+
+class Rect(Element): 
+    def __init__(self, dx, dy):
+        Element.__init__(self, 'rect')
+        self.dx = dx
+        self.dy = dy
+
+class Line(Element):
+    def __init__(self, p1, p2):
+        Element.__init__(self, 'line')
+        (self.x1, self.y1) = p1
+        (self.x2, self.y2) = p1
+        self.w = 0.1
